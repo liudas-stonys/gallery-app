5b54ff188c8f0123bdbe975b40c2402576ac244b
Liudas Stonys <liudas.stonys@x.insoft.lt>
2019-11-07 09:31:09 +0200

    phew work commit

--- gallery-bl/src/main/java/lt/liudasstonys/galleryapp/seeder/DbSeeder.java
+++ gallery-bl/src/main/java/lt/liudasstonys/galleryapp/seeder/DbSeeder.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 @RequiredArgsConstructor
@@ -21,21 +23,21 @@ public class DbSeeder {
     private final ImageRepository imageRepository;
     private final DbService dbService;
     private final String[] filesPath = {
-//            "../gallery-bl/src/main/java/lt/liudas/assets/1.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/2.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/3.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/4.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/5.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/6.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/7.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/8.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/9.jpeg",
-//            "../gallery-bl/src/main/java/lt/liudas/assets/10.jpeg",
-            "../gallery-bl/src/main/java/lt/liudas/assets/17.jpeg"
+            "classpath:assets/1.jpeg",
+//            "classpath:assets/2.jpeg",
+            "classpath:assets/3.jpeg",
+            "classpath:assets/4.jpeg",
+//            "classpath:assets/5.jpeg",
+            "classpath:assets/6.jpeg",
+            "classpath:assets/7.jpeg",
+            "classpath:assets/8.jpeg",
+            "classpath:assets/9.jpeg",
+            "classpath:assets/10.jpeg"
+//            "classpath:assets/17.jpeg"
     };
 
     @EventListener
-    public void seed(ContextRefreshedEvent event) throws IOException {
+    public void seedDb(ContextRefreshedEvent event) throws IOException {
         seedImageTable();
 //        seedCategoryTable();
 //        seedTagsTable();
@@ -47,46 +49,40 @@ public class DbSeeder {
 //        imageRepository.save(new Image());
 //        imageRepository.save(new Image());
 //        imageRepository.save(new Image());
-//
 //        DbSeeder.log.info("Images found with findAll():");
 //        DbSeeder.log.info("-------------------------------");
 //        for (Image image : imageRepository.findAll()) {
 //            DbSeeder.log.info(image.getId().toString() + ": " + image.toString());
 //        }
 //        DbSeeder.log.info("");
-//
 //        DbSeeder.log.info("Image found with findById(1L):");
 //        DbSeeder.log.info("--------------------------------");
 //        Optional<Image> image = imageRepository.findById(1L);
 //        DbSeeder.log.info(image.toString());
 //        DbSeeder.log.info("");
 
-//        https://howtodoinjava.com/spring-boot2/read-file-from-resources/
-//        https://www.mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        File file = resourceLoader.getResource("classpath:17.jpeg").getFile();
-        System.out.println(file.getPath());
-
+//        File file = resourceLoader.getResource("classpath:17.jpeg").getFile();
+//        System.out.println(file.getPath());
 //        System.out.println(Path.of("classpath:17.jpeg"));
-
 //        System.out.println(System.getProperty("user.dir"));
 //        System.out.println(new File(".").getCanonicalPath());
-
 //        Files.readAllBytes(Path.of("C:/Users/liudas.stonys/IdeaProjects/gallery-app/gallery-bl/src/main/java/lt/liudasstonys/galleryapp/assets/17.jpeg"));
 //        Files.readAllBytes(Path.of("assets/17.jpeg"));
 //        Files.readAllBytes(Path.of("17.jpeg"));
 
-//        for (String filePath : filesPath) {
-//            File file = new File(filePath);
-//            Path path = Path.of(filePath);
-//
-//            String title = file.getName();
-//            String mime = Files.probeContentType(path);
-//            byte[] data = Files.readAllBytes(path);
-//            Long size = file.length();
-//            String description = file.toString();
-//
-////            dbService.saveImage(title, mime, data, size, description);
-//        }
+        // https://howtodoinjava.com/spring-boot2/read-file-from-resources/
+        // https://www.mkyong.com/java/java-read-a-file-from-resources-folder/
+        for (String filePath : filesPath) {
+            File file = resourceLoader.getResource(filePath).getFile();
+            Path path = Path.of(file.getPath());
+
+            String title = file.getName();
+            String mime = Files.probeContentType(path);
+            byte[] data = Files.readAllBytes(path);
+            Long size = file.length();
+            String description = file.toString();
+
+            dbService.saveImage(title, mime, data, size, description);
+        }
     }
 }
--- gallery-model/src/main/java/lt/liudasstonys/galleryapp/entity/Image.java
+++ gallery-model/src/main/java/lt/liudasstonys/galleryapp/entity/Image.java
@@ -1,22 +1,39 @@
 package lt.liudasstonys.galleryapp.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 
 // TODO: The primary key variable that is declared within the bean class must be declared as public. ???
 // https://docs.oracle.com/cd/E16439_01/doc.1013/e13981/undejbs003.htm
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 @Getter
 @Setter
 @ToString(exclude = "data")
 public class Image extends BaseEntity {
     private String title;
     private String mime;
+//    @Lob
+    @Column(name = "data", columnDefinition="blob")
     private byte[] data;
     private Long size;
     private String description;
+
+    public Image(String title, String mime, byte[] data, Long size, String description) {
+        this.title = title;
+        this.mime = mime;
+        this.data = data;
+        this.size = size;
+        this.description = description;
+    }
 }
